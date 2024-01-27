2e487b3ee5734f3ba26a4649adceeb30255ce3c8
yjp0619 <lisajj0619@gmail.com>
2024-01-27 17:27:56 +0900

    team04

--- task_yujin_park_4/photos_class.py
+++ task_yujin_park_4/photos_class.py
@@ -1,8 +1,14 @@
 class Photo:
-    def __init__(self, id, album_id, title, url, thumbnail_url):
+    def __init__(self, id: int, album_id: int, title: str, url: str, thumbnail_url: str):
         self.id = id
         self.album_id = album_id
         self.title = title
         self.url = url
         self.thumbnail_url = thumbnail_url
 
+
+class Album:
+    def __init__(self,id: int, user_id: int, title:str):
+        self.id = id
+        self.user_id = user_id
+        self.title = title
