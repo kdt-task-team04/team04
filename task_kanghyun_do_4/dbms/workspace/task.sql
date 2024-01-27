f951ecd973d6a736f2f6d382c2f4d1f900fb3007
Respec-Do <kanghyundo@gmail.com>
2024-01-27 21:32:23 +0900

    sql 작업중

---
+++ task_kanghyun_do_4/dbms/workspace/task.sql
@@ -0,0 +1,20 @@
+create table tbl_post(
+    id bigint auto_increment primary key,
+    title varchar(255) not null,
+    body varchar(255) not null,
+
+
+);
+
+create table tbl_user(
+    id bigint auto_increment primary key,
+    post_id bigint not null,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    content varchar(255) not null,
+    constraint fk_comment_post foreign key(post_id)
+                        references tbl_post(id)
+);
+
+select * from tbl_post;
+
