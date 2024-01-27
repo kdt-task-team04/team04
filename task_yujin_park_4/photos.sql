3152c5ad34d0e5affc94b23b102ed35bf7e30ede
yjp0619 <lisajj0619@gmail.com>
2024-01-27 15:48:41 +0900

    team04

--- task_yujin_park_4/photos.sql
+++ task_yujin_park_4/photos.sql
@@ -1,17 +1,29 @@
-create table tbl_Album(
+create table tbl_users(
     id bigint auto_increment primary key,
-    user_id bigint not null,
+    name varchar(255) not null,
+    username varchar(255) not null,
+    email varchar(255) not null,
+    phone varchar(255) not null,
+    website varchar(255) not null
+);
+
+create table tbl_album(
+    id bigint auto_increment primary key,
     title varchar(255) not null
+    user_id bigint not null,
+    constraint fk_album_user foreign key (user_id)
+    references tbl_user(id)
+
 );
 
 create table tbl_photos(
-    id          bigint auto_increment primary key,
-    title       varchar(255) not null,
-    url         varchar(255) not null,
-    thumbnaiUrl varchar(255) not null,
-    Album_id bigint not null,
-    constraint fk_photos_Album foreign key(Album_id)
-        references tbl_album (id)
+    id bigint auto_increment primary key,
+    title varchar(255) not null,
+    url varchar(255) not null,
+    thumbnail_url varchar(255) not null,
+    album_id bigint not null,
+    constraint fk_photos_album foreign key(album_id)
+    references tbl_album (id)
 );
 
 select * from tbl_photos;
---
+++ task_yujin_park_4/photos_class.py
@@ -0,0 +1,8 @@
+class Photo:
+    def __init__(self, id, album_id, title, url, thumbnail_url):
+        self.id = id
+        self.album_id = album_id
+        self.title = title
+        self.url = url
+        self.thumbnail_url = thumbnail_url
+
