0380e154b5f3e1f6fbaaf0e55d3a79da6eb402f7
Respec-Do <kanghyundo@gmail.com>
2024-01-29 02:03:22 +0900

    수정 작업

--- task_kanghyun_do_4/post_main.py
+++ task_kanghyun_do_4/post_main.py
@@ -1,4 +1,5 @@
 from crud_module import *
+from user import User
 
 if __name__ == '__main__':
      # 게시글 추가
@@ -12,24 +13,24 @@ if __name__ == '__main__':
         (user.get('id'), '테스트 제목1','테스트 내용1'),
         (user.get('id'), '테스트 제목2','테스트 내용2'),
         (user.get('id'), '테스트 제목3','테스트 내용3'),
-        (user.get('id'), '테스트 제목4','테스트 내용4'),
+        (user.get('id'), '테스트 제목4','테스트 내용4')
     )
-     save_many(save_many_query, save_many_params)
+    save_many(save_many_query, save_many_params)
 
-     # 게시글 수정
-    find_by_id_query = "select * from tbl_post where id = %s"
-    find_by_id_params = 2,
-
-    post = find_by_id(find_by_id_query, find_by_id_params)
-    post['body'] = '수정된 테스트내용'
-    update_query = "update tbl_post \
-                    set body = %s \
-                    where id = %s"
-    update_params = (post.get('body'), post.get('id'))
-    update(update_query, update_params)
-
-    #댓글 삭제
-    delete_query = "delete from tbl_post where id = %s"
-    delete_params = 3,
-
-    delete(delete_query, delete_params)
+    #  # 게시글 수정
+    # find_by_id_query = "select * from tbl_post where id = %s"
+    # find_by_id_params = 2,
+    #
+    # post = find_by_id(find_by_id_query, find_by_id_params)
+    # post['body'] = '수정된 테스트내용'
+    # update_query = "update tbl_post \
+    #                 set body = %s \
+    #                 where id = %s"
+    # update_params = (post.get('body'), post.get('id'))
+    # update(update_query, update_params)
+    #
+    # #댓글 삭제
+    # delete_query = "delete from tbl_post where id = %s"
+    # delete_params = 3,
+    #
+    # delete(delete_query, delete_params)
