3fd051df2dc366f8b3f48a8a07e1ec9e70bc288a
Respec-Do <kanghyundo@gmail.com>
2024-01-29 18:42:01 +0900

    post join 작업 완료

---
+++ task_kanghyun_do_4/address.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(self, id, street, suite, city, zip_code, users):
+        self.id = id
+        self.street = street
+        self.suite = suite
+        self.city = city
+        self.zip_code = zip_code
+        self.users = users
